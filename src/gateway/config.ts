401fb3c8ea34dd91eb78bcd53d14f881e0339dfd
Jackson Dorsett <dorsettj@gmail.com>
2024-07-14 08:29:44 -0700

    fixed dev environment variables

--- src/gateway/config.ts
+++ src/gateway/config.ts
@@ -5,17 +5,15 @@ loadEnv({ path: "./dev.env" });
 
 interface EnvVars extends CleanedEnvAccessors {
   SERIES_INFO_URL: string;
-  // STORAGE_SERVICE_URL: string;
-  // EXTRACTION_SERVICE_URL: string;
-  // SERIES_PARSING_SERVICE_URL: string;
+  STORAGE_SERVICE_URL: string;
+  EXTRACTION_SERVICE_URL: string;
   PORT: number;
 }
 
 export const env = cleanEnv(process.env, {
   SERIES_INFO_URL: str(),
-  // STORAGE_SERVICE_URL: str(),
-  // EXTRACTION_SERVICE_URL: str(),
-  // SERIES_PARSING_SERVICE_URL: str(),
+  STORAGE_SERVICE_URL: str(),
+  EXTRACTION_SERVICE_URL: str(),
   PORT: port(),
 }) as EnvVars;
 
