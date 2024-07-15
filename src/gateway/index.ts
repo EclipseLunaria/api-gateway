5dab47bfc3edbacbdb1c56b5fa760b0b1bdb8a7c
Jackson Dorsett <dorsettj@gmail.com>
2024-07-15 09:19:31 -0700

    moved series router to index

--- src/gateway/index.ts
+++ src/gateway/index.ts
@@ -1,10 +1,11 @@
-import express from 'express';
-
+import express from "express";
+import { seriesRouter } from "./routes/seriesInfoRoutes";
 const app = express();
 app.use(express.json());
+app.use("/series", seriesRouter);
 
-app.get('/', (req, res) => {
-    res.send('api gateway is working');
+app.get("/", (req, res) => {
+  res.send("api gateway is working");
 });
 
 export default app;
--- src/gateway/server.ts
+++ src/gateway/server.ts
@@ -1,8 +1,7 @@
 import app from ".";
-import { seriesRouter } from "./routes/seriesInfoRoutes";
 
 const PORT = process.env.PORT || 6969;
-app.use("/series", seriesRouter);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
