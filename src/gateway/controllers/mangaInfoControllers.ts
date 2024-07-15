a65816d0b621fdcdc64b486319c9abdf0c334a68
Jackson Dorsett <dorsettj@gmail.com>
2024-07-15 11:15:33 -0700

    added manga series search

--- src/gateway/controllers/mangaInfoControllers.ts
+++ src/gateway/controllers/mangaInfoControllers.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from "express";
 import { normalizeSearchTerm } from "../utils/searchUtils";
+import { searchManga } from "../services/searchMangaServices";
 
-const mangaSearchController = (req: Request, res: Response) => {
+const mangaSearchController = async (req: Request, res: Response) => {
   const { term } = req.query;
   if (!term) {
     res.status(404).send({ message: "specify a search term" });
     return;
   }
   const normalizedSearchTerm = normalizeSearchTerm(term.toString());
-  
-  return res.send(term);
+  const searchResults = await searchManga(normalizedSearchTerm);
+  console.log(searchResults);
+  if (!searchResults) return res.status(404).send("Error");
+  return res.json(searchResults);
 };
 
 export { mangaSearchController };
--- src/gateway/services/searchMangaServices.ts
+++ src/gateway/services/searchMangaServices.ts
@@ -1,5 +1,17 @@
-const searchManga = async (mangaId: string) => {
-  return;
+import axios from "axios";
+import env from "../config";
+import { normalizeSearchTerm } from "../utils/searchUtils";
+const searchManga = async (searchTerm: string) => {
+  const normalizedSearchTerm = normalizeSearchTerm(searchTerm);
+  const endpointUrl = `${env.SERIES_INFO_URL}/search?term=${normalizedSearchTerm}`;
+  console.log(endpointUrl);
+  try {
+    const results = (await axios.get(endpointUrl)).data;
+    return results;
+  } catch (error: any) {
+    null;
+  }
+  return "hello";
 };
 
 export { searchManga };
