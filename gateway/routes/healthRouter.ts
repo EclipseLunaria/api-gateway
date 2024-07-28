67201df943545b146a2ffc38c2c2207cc2eb3b77
Jackson Dorsett <dorsettj@gmail.com>
2024-07-28 12:45:29 -0700

    removed depreciated utils

--- gateway/routes/healthRouter.ts
+++ gateway/routes/healthRouter.ts
@@ -1,30 +1,7 @@
 import { Router } from "express";
-import { buildServiceUrl } from "../utils";
-import axios from "axios";
-import env from "../config";
 import { getMicroserviceHealth } from "../controllers/healthControllers";
 
 const healthRouter = Router();
-
-healthRouter.get("/status/", async (req, res) => {
-  let html: string | null = null;
-  try {
-    const endpointPath = buildServiceUrl(
-      `http://localhost:${env.SERIES_INFO_PORT}`,
-      "/status"
-    );
-    html = (await axios.get(endpointPath)).data;
-    if (!html) {
-      throw Error("No response from series api");
-    }
-    res.status(200).send(html);
-  } catch (e: any) {
-    console.error(
-      `Error fetching status: ${e}, ${typeof e}, ${Object.entries(e)}`
-    );
-    res.status(500).send(`Error fetching status: ${e}`);
-  }
-});
 
 healthRouter.get("/microservices/", getMicroserviceHealth);
 
