d82e9903109945109c7b32a7ecf91caa19305fad
Jackson Dorsett <dorsettj@gmail.com>
2024-07-28 11:41:00 -0700

    changed default port

--- gateway/server.ts
+++ gateway/server.ts
@@ -1,7 +1,6 @@
 import app from ".";
 
-const PORT = process.env.PORT || 6969;
-
+const PORT = process.env.PORT || 6900;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
