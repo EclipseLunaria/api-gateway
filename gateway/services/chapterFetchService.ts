189828f5c53fe58a8899d70a35140db2c9d0467c
Jackson Dorsett <dorsettj@gmail.com>
2024-07-31 15:27:28 -0700

    updated submodules and chapterfetch port

--- gateway/services/chapterFetchService.ts
+++ gateway/services/chapterFetchService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import env from "../config";
-const BASE_URL = `http://localhost:${env.SERIES_INFO_PORT}`;
+const BASE_URL = `http://localhost:${env.DISTRIBUTION_PORT}`;
 export const fetchChapter = async (mangaId: string, chapterId: string) => {
   console.log(BASE_URL);
   console.log(`${BASE_URL}/fetch/manga-${mangaId}/chapter-${chapterId}`);
