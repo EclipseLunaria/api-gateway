e510d5a37c42a51ce6ebb341ecd8f121d61607cd
Jackson Dorsett <dorsettj@gmail.com>
2024-08-01 19:44:26 -0700

    change scheme

--- gateway/services/chapterFetchService.ts
+++ gateway/services/chapterFetchService.ts
@@ -5,7 +5,7 @@ export const fetchChapter = async (mangaId: string, chapterId: string) => {
   console.log(BASE_URL);
   console.log(`${BASE_URL}/fetch/manga/${mangaId}/chapter/${chapterId}`);
   const response = await axios.get(
-    `${BASE_URL}/fetch/manga-${mangaId}/chapter-${chapterId}`
+    `${BASE_URL}/fetch/manga/${mangaId}/chapter/${chapterId}`
   );
   return response.data;
 };
