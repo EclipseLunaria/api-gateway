eb7b87fa0e9ccf83a55c5fe1db4ae08d59e8193f
Jackson Dorsett <dorsettj@gmail.com>
2024-08-10 11:36:31 -0700

    fix

--- pm2.config.js
+++ pm2.config.js
@@ -4,11 +4,12 @@ module.exports = {
   apps: [
     {
       name: "gateway",
-      script: path.join(__dirname, "node_modules/.bin/ts-node"),
-      args: "server.ts",
-      cwd: path.join(__dirname, "gateway"),
+      script: "server.ts", // Specify the entry file
+      interpreter: "npx", // Use 'npx' as the interpreter
+      interpreterArgs: "ts-node", // Use 'ts-node' as an argument for the interpreter
+      cwd: path.join(__dirname, "gateway"), // Ensure this path is correct
       watch: true, // Enable watch mode
-      ignore_watch: ["node_modules"],
+      ignore_watch: ["node_modules"], // Ignore changes in node_modules
       env: {
         NODE_ENV: "production",
       },
