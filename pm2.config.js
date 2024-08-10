1f646dd622148f9db6ea29c3834756de1cc5aa2c
Jackson Dorsett <dorsettj@gmail.com>
2024-08-10 11:22:14 -0700

    added

--- pm2.config.js
+++ pm2.config.js
@@ -4,9 +4,9 @@ module.exports = {
   apps: [
     {
       name: "gateway",
-      script: `${path.join(__dirname, "node_modules/.bin/ts-node")}`,
+      script: `ts-node`,
       args: "./server.ts",
-      cwd: path.join(__dirname, "gateway"),
+      // cwd: path.join(__dirname, "gateway"),
       watch: true, // Enable watch mode
       ignore_watch: ["node_modules"],
       env: {
