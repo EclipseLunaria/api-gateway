3702956dea4bb614d08d834266344aaf7ad8ed1f
Jackson Dorsett <dorsettj@gmail.com>
2024-08-15 11:20:24 -0700

    added get series list service

--- gateway/services/searchMangaServices.ts
+++ gateway/services/searchMangaServices.ts
@@ -1,10 +1,29 @@
 import axios from "axios";
 import env from "../config";
 import { normalizeQuery } from "../utils/searchUtils";
+import { SearchCategory } from "../types";
+
 const BASE_URL = `http://localhost:${env.SERIES_INFO_PORT}`;
 const searchMangaSeries = async (q: string) => {
   const query = normalizeQuery(q);
   const endpointUrl = `${BASE_URL}/search?q=${query}`;
+  console.log(endpointUrl);
+  try {
+    const results = (await axios.get(endpointUrl)).data;
+    return results;
+  } catch (error: any) {
+    console.log(error);
+    return null;
+  }
+};
+
+const getSeriesList = async (type: SearchCategory) => {
+  const endpoints = {
+    last_updated: `${BASE_URL}/latest/updated`,
+    popular: `${BASE_URL}/most/popular`,
+    newest: `${BASE_URL}/newest`,
+  };
+  const endpointUrl = endpoints[type];
   console.log(endpointUrl);
   try {
     const results = (await axios.get(endpointUrl)).data;
@@ -56,6 +75,7 @@ const fetchMangaFields = async (mangaId: string, fieldId: string) => {
 
 export {
   searchMangaSeries,
+  getSeriesList,
   fetchMangaInfo,
   fetchChapterList,
   fetchMangaFields,
