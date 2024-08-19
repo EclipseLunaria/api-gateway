f4d82c9162da1500c6d95410d4ac6ae2ac96408b
Jackson Dorsett <dorsettj@gmail.com>
2024-08-18 17:10:48 -0700

    moved to .env

--- gateway/config.ts
+++ gateway/config.ts
@@ -1,23 +1,14 @@
 import path from "path";
 import { config as loadEnv } from "dotenv";
-import { isDev } from "./utils";
+import "dotenv/config";
 
-const envPath = path.resolve(
-  __dirname,
-  `${isDev() ? "../local.env" : "../production.env"}`
-);
-
-loadEnv({ path: envPath });
-const config = loadEnv({ path: envPath }).parsed;
-if (!config) {
-  throw new Error(`No config found at: ${envPath}`);
-}
 const env = {
-  SERIES_INFO_PORT: config.SERIES_INFO_PORT || 6901,
-  STORAGE_PORT: config.STORAGE_PORT || 6902,
-  DISTRIBUTION_PORT: config.DISTRIBUTION_PORT || 6903,
-  GATEWAY_PORT: config.GATEWAY_PORT || 6900,
-  DATA_LAYER_PORT: config.DATA_LAYER_PORT || 6999,
+  SERIES_INFO_PORT: process.env.SERIES_INFO_PORT || 6901,
+  STORAGE_PORT: process.env.STORAGE_PORT || 6902,
+  DISTRIBUTION_PORT: process.env.DISTRIBUTION_PORT || 6903,
+  GATEWAY_PORT: process.env.GATEWAY_PORT || 6900,
+  DATA_LAYER_PORT: process.env.DATA_LAYER_PORT || 6999,
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
 };
 
 export default env;
