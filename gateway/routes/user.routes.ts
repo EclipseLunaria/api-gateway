ece08a106661da6139287885e96f3f3ce3d0c6d1
eclipse-lunaria-deploy <jackson@eclipselunaria.dev>
2024-08-25 23:00:01 +0000

    added user route

--- gateway/routes/user.routes.ts
+++ gateway/routes/user.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth.middleware";
-import IAuthenticatedRequest from "../middlewares/AuthenticatedRequest.interface";
+import IAuthenticatedRequest from "../interfaces/auth.interfaces/AuthenticatedRequest.interface";
 const userRouter = Router();
 userRouter.use(authMiddleware);
 userRouter.get("/", (req, res) => {
