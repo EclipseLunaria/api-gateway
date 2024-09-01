455469c7aabe48cb60da58a81f058c69cee71945
Jackson Dorsett <dorsettj@gmail.com>
2024-09-01 11:33:51 -0700

    add random route and remove old series services

--- gateway/routes/series.routes.ts
+++ gateway/routes/series.routes.ts
@@ -1,11 +1,21 @@
-import { Router } from "express";
+import { Router, Response, Request, NextFunction } from "express";
 import {
   infoController,
   fieldController,
   chapterController,
+  randomSeriesController,
 } from "../controllers/series.controllers";
-
 const seriesRouter = Router();
+
+seriesRouter.param(
+  "mangaId",
+  (req: Request, res: Response, next: NextFunction, mangaId) => {
+    (req as any).mangaId = mangaId;
+    next();
+  }
+);
+
+seriesRouter.get("/random", randomSeriesController);
 
 seriesRouter.get("/:mangaId/", infoController);
 seriesRouter.get("/:mangaId/chapters", chapterController);
